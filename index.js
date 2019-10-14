eb0cc3e1026636dfa33da64af17a4bb1a55ff97a
Sindre Sorhus <sindresorhus@gmail.com>
2019-10-14 19:38:10 +0700

    Meta tweaks

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const userAgent = process.env.npm_config_user_agent;
 const isYarn = Boolean(userAgent && userAgent.startsWith('yarn'));
 const isNpm = Boolean(userAgent && userAgent.startsWith('npm'));
 
-// TODO: This named export should be replaced by a default export as soon as we move to ES modules
-exports.isNpmOrYarn = isNpm || isYarn;
-exports.isNpm = isNpm;
-exports.isYarn = isYarn;
+module.exports.isNpmOrYarn = isNpm || isYarn;
+module.exports.isNpm = isNpm;
+module.exports.isYarn = isYarn;
